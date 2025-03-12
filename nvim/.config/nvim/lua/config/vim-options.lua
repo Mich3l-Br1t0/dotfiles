4ac01e2227be68e2ff6266484a9a65a76db4a768
Michel Brito <michelllbrito1410@gmail.com>
2025-03-12 10:09:17 -0300

    Initial commit

---
+++ nvim/.config/nvim/lua/config/vim-options.lua
@@ -0,0 +1,19 @@
+vim.g.mapleader = " "
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.wrap = false
+vim.opt.smartindent = true
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.updatetime = 50
+
